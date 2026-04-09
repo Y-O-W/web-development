665f9f141908f8f21d05a475281b16b455945b6a
YOW <yves@yowfactor.com>
2026-04-09 12:00:17 +0200

    added Ruby code snippets 20260409

---
+++ ruby/script.rb
@@ -0,0 +1,28 @@
+# This is a learining exercise with some basic ruby code snippets
+
+#Variable - Local
+
+name = "Yves Wilken"
+first_name = "Yves"
+last_name = "Wilken"
+
+puts "#{last_name} #{first_name}"
+puts name
+
+my_name = "Yves Wilken"
+my_age = 30
+
+puts "My name is #{my_name} and I am #{my_age} year old."
+
+sum = 13 + 379
+product = 923 * 15
+quotient = 13209 / 17
+
+puts sum
+puts product
+puts quotient
+
+#Object Oriented Programming - Property & Action
+#In Ruby properties are know as instance variables and actions are known as methods.
+
+5.times { print "We *love* Ruby -- it's outrageous!" }
